38792189b7a569e1c5bdd3e1fbbe03ad5f78967b
artyom250 <artyom.agadzhanyan250@gmail.com>
2024-02-14 14:25:55 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSkills() {
+    document.getElementById("projects").style.display="none";
+    document.getElementById("skills").style.display="flex";
+}
+
+function showProjects() {
+    document.getElementById("projects").style.display="grid";
+    document.getElementById("skills").style.display="none";
+}
+
